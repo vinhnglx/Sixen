36a44be058eb5042717e7938dfe90983c27e2ed5
Vinh Nguyen <vinh.nglx@gmail.com>
2015-10-12 00:31:42 +0700

    Config for twitter

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,16 @@
+# Initialize a Twitter Rest Client
+#
+# Examples
+#
+#   client = TwitterAPI.new
+#
+class TwitterAPI
+  def initialize
+    Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+    end
+  end
+end
